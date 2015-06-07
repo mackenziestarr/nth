f95e4dc999c3ea9063092198570199edfa2163eb
Mackenzie Starr <mackestarr@gmail.com>
2015-06-06 20:53:53 -0400

    Initial Commit

---
+++ impulse/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  impulse
+//
+//  Created by Mackenzie Starr on 6/6/15.
+//  Copyright (c) 2015 Mackenzie Starr. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
