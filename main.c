6e94e4df5443775aab9d617531bf1d0c716dab98
Mackenzie Starr <mackestarr@gmail.com>
2015-06-20 22:38:09 -0400

    sync i/o multiplexing and raw terminal for keypress events

--- main.c
+++ main.c
@@ -1,12 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
+#include <sys/signal.h>
+#include <sys/select.h>
 #include <portaudio.h>
+#include <termios.h>
+#include <unistd.h>
 #include "waveform.h"
+
+#define NB_ENABLE (0)
+#define NB_DISABLE (1)
 
 static int run = 1;
 void handle(int noop) {
     run = 0;
+}
+
+int keypress () {
+    fd_set buffer;
+    struct timeval time;
+    // poll
+    time.tv_sec = 0;
+    time.tv_usec = 0;
+    FD_ZERO(&buffer);
+    FD_SET(0, &buffer);
+    select(1, &buffer, NULL, NULL, &time);
+    return FD_ISSET(0, &buffer);
+}
+
+void raw_tty(int state) {
+    struct termios ttystate;
+    // get state
+    tcgetattr(0, &ttystate);
+    if (state == NB_ENABLE) {
+        ttystate.c_lflag &= ~ICANON;
+        ttystate.c_lflag &= ~ECHO;
+        ttystate.c_cc[VMIN] = 1;
+    } 
+    else if (state == NB_DISABLE) {
+        ttystate.c_lflag |= ICANON;
+        ttystate.c_lflag |= ECHO;
+    }
+    tcsetattr(0, TCSANOW, &ttystate);
 }
 
 void end(PaError error) {
@@ -44,12 +78,22 @@ int main() {
     if (error != paNoError) end(error); 
     
     Pa_StartStream(stream);
-
-    while(run);
+    
+    raw_tty(NB_ENABLE);
+    
+    int keycode = 0;
+    char key;
+    while(run) {
+        keycode = keypress();
+        if (keycode != 0) {
+            key = fgetc(stdin);
+            fprintf(stderr, "you pressed %c\n", key);
+        }
+    };
     
     Pa_StopStream(stream);
     Pa_CloseStream(stream);
     end(error);
-    
+    raw_tty(NB_DISABLE);    
     return 0;
 }
